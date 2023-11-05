fab908f72360e5224fad5d5b2e82b218c37b5201
Ryan McFall <mcfall@hope.edu>
2023-11-05 15:32:09 -0500

    Add Lombok and modify Actor to use it

--- src/main/java/edu/hope/cs/csci392/imdb/model/Actor.java
+++ src/main/java/edu/hope/cs/csci392/imdb/model/Actor.java
@@ -1,7 +1,15 @@
 package edu.hope.cs.csci392.imdb.model;
 
-public class Actor {
-	
+import lombok.experimental.Accessors;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Accessors(chain = true)
+@NoArgsConstructor
+@AllArgsConstructor
+public class Actor {	
 	private String personID;
 	private String firstName;
 	private String middleName;
@@ -9,60 +17,5 @@ public class Actor {
 	private String suffix;
 	private String fullName;
 	private int birthYear;
-	private int deathYear;
-	
-	public Actor (
-		String personID,
-		String firstName, String middleName, String lastName, String suffix, String fullName,
-		int birthYear, int deathYear
-	) {
-		this.personID = personID;
-		this.fullName = fullName;
-		this.firstName = firstName;
-		this.middleName = middleName;
-		this.lastName = lastName;	
-		this.suffix = suffix;
-		this.birthYear = birthYear;
-		this.deathYear = deathYear;
-	}
-
-	public String getStageFirstName() {
-		return firstName;
-	}
-
-	public String getStageLastName() {
-		return lastName;
-	}
-
-	public String getPersonID() {
-		return personID;
-	}
-	
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public String getMiddleName() {
-		return middleName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public String getSuffix() {
-		return suffix;
-	}
-
-	public String getFullName() {
-		return fullName;
-	}
-
-	public int getBirthYear() {
-		return birthYear;
-	}
-
-	public int getDeathYear() {
-		return deathYear;
-	}
+	private int deathYear;		
 }
